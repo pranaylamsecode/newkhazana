05e57b7ee91758256f80e31c4e68e326c5942944
pranay-lamse <pranay.l@veerit.com>
2024-10-03 17:34:02 +0530

    added

--- resources/views/theme/home.blade.php
+++ resources/views/theme/home.blade.php
@@ -1,28 +1,25 @@
 @extends('theme.layouts.app')
 
 @section('content')
-
-<div class="row">
-  <div class="col-md-12">
-    @if (session('status'))
-      <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-      </div>
-    @endif
-  </div>
-
-  @foreach($dashboard_cards as $card)
-    <div class="col-md-6 col-lg-3">
-      <a href="{{ ($card[2]) }}" class="card-hover">
-        <div class="widget-small primary coloured-icon">
-          <i class="icon {{ $card[3] }} fa-3x"></i>
-          <div class="info">
-            <h4>{{ $card[0] }}</h4>
-            <p><b><?php echo number_format($card[1]); ?></b></p>
-          </div>
+    <div class="row">
+        <div class="col-md-12">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
         </div>
-      </a>
+
+        @foreach ($dashboard_cards as $card)
+            <div class="col-md-6 col-lg-3">
+                <a href="#" class="card-hover">
+
+                    <?php $today = \Carbon\Carbon::now('Asia/Kolkata')->format('Y-m-d'); ?>
+                    <h4>Today's : {{ \Carbon\Carbon::parse($today)->format('l') }} {{ $today }}</h4><br />
+                    <h2>Result </h2>
+
+                </a>
+            </div>
+        @endforeach
     </div>
-  @endforeach
-</div>
 @endsection
