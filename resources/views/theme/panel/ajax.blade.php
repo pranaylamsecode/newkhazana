dd68f1b59d9a7e35a8875958273ef66c84125945
pranay-lamse <pranay.l@veerit.com>
2024-10-08 19:09:05 +0530

    added

--- resources/views/theme/panel/ajax.blade.php
+++ resources/views/theme/panel/ajax.blade.php
@@ -2,126 +2,107 @@
     @php
         $record_id = $offset;
     @endphp
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                {{-- <th width="10px">
-                    <input type="checkbox" name="row_check_all" class="row_check_all">
-                </th> --}}
-                <th>Date</th>
-                <th>Day</th>
-                <th>Number</th>
-                <th>Category</th>
-                <th>Status</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data as $v)
-                <tr class="row_{{ $v->id }}">
-                    <td>{{ $v->name }}</td>
-                    <td>{{ \Carbon\Carbon::parse($v->name)->format('l') }}</td>
-                    <td>{{ $v->number }}</td>
-                    <td>{{ $v->category_name }}</td>
-                    <td>
-                        @if ($v->status == 1)
-                            <span style="color: green;">●</span> {{-- Green dot for status 1 --}}
-                        @else
-                            <span style="color: red;">●</span> {{-- Red dot for any other status --}}
-                        @endif
-                    </td>
-
-
-
-
-
-                    <td>
-                        <a href="#" data-id="{{ $v->id }}"
-                            class="btn btn-danger delete_btn delete{{ $v->id }} btn-sm">Delete</a>
-                    </td>
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    {{-- <th width="10px">
+                        <input type="checkbox" name="row_check_all" class="row_check_all">
+                    </th> --}}
+                    <th>Date</th>
+                    <th>Day</th>
+                    <th>Number</th>
+                    <th>Category</th>
+                    <th>Status</th>
+                    <th></th>
                 </tr>
-                <?php $page_number++; ?>
-            @endforeach
-        <tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($data as $v)
+                    <tr class="row_{{ $v->id }}">
+                        <td>{{ $v->name }}</td>
+                        <td>{{ \Carbon\Carbon::parse($v->name)->format('l') }}</td>
+                        <td>{{ $v->number }}</td>
+                        <td>{{ $v->category_name }}</td>
+                        <td>
+                            @if ($v->status == 1)
+                                <span style="color: green;">●</span>
+                            @else
+                                <span style="color: red;">●</span>
+                            @endif
+                        </td>
+                        <td>
+                            <a href="#" data-id="{{ $v->id }}"
+                                class="btn btn-danger delete_btn delete{{ $v->id }} btn-sm">Delete</a>
+                        </td>
+                    </tr>
+                    <?php $page_number++; ?>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @else
-    <div class="alert alert-warning" align="center">
-        Opps, seems like records not available.
+    <div class="alert alert-warning text-center">
+        Oops, seems like records are not available.
     </div>
 @endif
 
 @if ($pagination['total_records'] > $pagination['item_per_page'])
     <div class="card-header">
         <div class="pl-3">
-            <div class="paging_simple_numbers">
-                <ul class="pagination">
+            <nav aria-label="Page navigation">
+                <ul class="pagination justify-content-center">
                     <?php
                     echo paginate_function($pagination['item_per_page'], $pagination['current_page'], $pagination['total_records'], $pagination['total_pages']);
                     ?>
                 </ul>
-            </div>
+            </nav>
         </div>
     </div>
 @endif
+
 <?php
 function paginate_function($item_per_page, $current_page, $total_records, $total_pages)
 {
     $pagination = '';
     if ($total_pages > 0 && $total_pages != 1 && $current_page <= $total_pages) {
-        //verify total pages and current page number
         $right_links = $current_page + 3;
-        $previous = $current_page - 3; //previous link
-        $next = $current_page + 1; //next link
-        $first_link = true; //boolean var to decide our first link
+        $previous = $current_page - 3;
+        $next = $current_page + 1;
+        $first_link = true;
 
         if ($current_page > 1) {
             $previous_link = $previous <= 0 ? 1 : $previous;
-            $pagination .= '<li class="page-item "><a class="paginate_link page-link"  href="#" aria-controls="datatable1" data-page="1" title="First">&laquo;</a></li>'; //first link
-            $pagination .= '<li class="page-item "><a class="paginate_link page-link"  href="#" aria-controls="datatable1" data-page="' . $previous_link . '" title="Previous">&lt;</a></li>'; //previous link
+            $pagination .= '<li class="page-item"><a class="paginate_link page-link" href="#" data-page="1" title="First">&laquo;</a></li>';
+            $pagination .= '<li class="page-item"><a class="paginate_link page-link" href="#" data-page="' . $previous_link . '" title="Previous">&lt;</a></li>';
+
             for ($i = $current_page - 2; $i < $current_page; $i++) {
-                //Create left-hand side links
                 if ($i > 0) {
-                    $pagination .= '<li class="page-item "><a class="paginate_link page-link"  href="#" data-page="' . $i . '" aria-controls="datatable1" title="Page' . $i . '">' . $i . '</a></li>';
+                    $pagination .= '<li class="page-item"><a class="paginate_link page-link" href="#" data-page="' . $i . '" title="Page' . $i . '">' . $i . '</a></li>';
                 }
             }
-            $first_link = false; //set first link to false
+            $first_link = false;
         }
 
         if ($first_link) {
-            //if current active page is first link
-            $pagination .=
-                '<li class="page-item active">
-        <a class="paginate_link page-link" aria-controls="datatable1">' .
-                $current_page .
-                '</a></li>';
+            $pagination .= '<li class="page-item active"><a class="paginate_link page-link">' . $current_page . '</a></li>';
         } elseif ($current_page == $total_pages) {
-            //if it's the last active link
-            $pagination .=
-                '<li class="page-item active">
-        <a class="paginate_link page-link" aria-controls="datatable1">' .
-                $current_page .
-                '</a></li>';
+            $pagination .= '<li class="page-item active"><a class="paginate_link page-link">' . $current_page . '</a></li>';
         } else {
-            //regular current link
-            $pagination .=
-                '<li class="page-item active">
-        <a class="paginate_link page-link" aria-controls="datatable1">' .
-                $current_page .
-                '</a></li>';
+            $pagination .= '<li class="page-item active"><a class="paginate_link page-link">' . $current_page . '</a></li>';
         }
 
         for ($i = $current_page + 1; $i < $right_links; $i++) {
-            //create right-hand side links
             if ($i <= $total_pages) {
-                $pagination .= '<li class="page-item "><a class="paginate_link page-link" href="#" aria-controls="datatable1" data-page="' . $i . '" title="Page ' . $i . '">' . $i . '</a></li>';
+                $pagination .= '<li class="page-item"><a class="paginate_link page-link" href="#" data-page="' . $i . '" title="Page ' . $i . '">' . $i . '</a></li>';
             }
         }
         if ($current_page < $total_pages) {
             $next_link = $i > $total_pages ? $total_pages : $i;
-            $pagination .= '<li class="page-item "><a class="paginate_link page-link" href="#" aria-controls="datatable1" data-page="' . $next_link . '" title="Next">&gt;</a></li>'; //next link
-            $pagination .= '<li class="page-item "><a class="paginate_link page-link" href="#" aria-controls="datatable1" data-page="' . $total_pages . '" title="Last">&raquo;</a></li>'; //last link
+            $pagination .= '<li class="page-item"><a class="paginate_link page-link" href="#" data-page="' . $next_link . '" title="Next">&gt;</a></li>';
+            $pagination .= '<li class="page-item"><a class="paginate_link page-link" href="#" data-page="' . $total_pages . '" title="Last">&raquo;</a></li>';
         }
     }
-    return $pagination; //return pagination links
+    return $pagination;
 }
 ?>
