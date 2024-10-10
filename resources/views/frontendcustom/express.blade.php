902abdaa8104ab7976d4590d6c79485ffcd8170c
pranay-lamse <pranay.l@veerit.com>
2024-10-10 17:19:46 +0530

    added

--- resources/views/frontendcustom/express.blade.php
+++ resources/views/frontendcustom/express.blade.php
@@ -1,119 +1,32 @@
 <!DOCTYPE html>
 <html amp lang="en-in">
-<!-- Mirrored from spboss.in/jodi/khajana.php by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 02 Oct 2024 05:45:36 GMT -->
-<!-- Added by HTTrack -->
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title> JODI CHART RECORD SATTA MATKA</title>
+    <title>JODI CHART RECORD SATTA MATKA</title>
     <meta name="description"
         content="KHAJANA CHART MATKA BAZAR CHART RECORD LIVE KHAJANA MATKA CHARTS RECORD WITH JODIES PANNA KHAJANA PATTI OLD MATKA RECORD MATKA JODI RECORD SATTA HISTORY" />
     <link rel="canonical" href="khajana.html" />
-
-    <style amp-boilerplate>
-        body {
-            -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-            -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-            -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-            animation: -amp-start 8s steps(1, end) 0s 1 normal both;
-        }
-
-        @-webkit-keyframes -amp-start {
-            from {
-                visibility: hidden;
-            }
-
-            to {
-                visibility: visible;
-            }
-        }
-
-        @-moz-keyframes -amp-start {
-            from {
-                visibility: hidden;
-            }
-
-            to {
-                visibility: visible;
-            }
-        }
-
-        @-ms-keyframes -amp-start {
-            from {
-                visibility: hidden;
-            }
-
-            to {
-                visibility: visible;
-            }
-        }
-
-        @-o-keyframes -amp-start {
-            from {
-                visibility: hidden;
-            }
-
-            to {
-                visibility: visible;
-            }
-        }
-
-        @keyframes -amp-start {
-            from {
-                visibility: hidden;
-            }
-
-            to {
-                visibility: visible;
-            }
-        }
-    </style>
-    <noscript>
-        <style amp-boilerplate>
-            body {
-                -webkit-animation: none;
-                -moz-animation: none;
-                -ms-animation: none;
-                animation: none;
-            }
-        </style>
-    </noscript>
     <link rel="shortcut icon" href="../fav/favicon.ico" />
-    <style amp-custom>
-        html {
-            scroll-behavior: smooth;
-        }
 
+    <style amp-custom>
         body {
             background-color: #fc9;
             text-align: center;
             font-weight: 700;
-            padding: 0;
             font-family: Helvetica, sans-serif;
+            margin: 0;
+            padding: 0;
         }
 
         .logo {
-            background: #fc9;
-            padding: 0 10px;
-            display: block;
-            color: #fff8f8;
+            padding: 10px;
+            color: #fff;
             margin-bottom: 5px;
-            letter-spacing: 1px;
             font-weight: 700;
             border: 3px solid #ff0016;
             border-radius: 0.75em;
-            transform-style: preserve-3d;
-            transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
-                background 150ms cubic-bezier(0, 0, 0.58, 1);
-        }
-
-        .logo amp-img {
-            width: 220px;
-            height: auto;
-            padding: 6px 0 0;
         }
 
         .button2 {
@@ -121,72 +34,33 @@
             color: #220c82;
             padding: 10px 30px;
             font-size: 14px;
-            margin: 0px 0px 5px 0px;
-            border: 2px solid #0000005c;
+            border: 2px solid #000;
             font-weight: 800;
             text-decoration: none;
-            text-shadow: 1px 1px #00bcd4;
-            box-shadow: 0 8px 10px 0 rgba(0, 0, 0, 0.2),
-                0 6px 8px 0 rgba(0, 0, 0, 0.19);
             display: inline-block;
             transition: all 0.3s;
         }
 
-        .ad-div11 {
-            text-align: center;
-            margin: 0 0 10px;
-        }
-
         .chart-list {
             border: 2px solid #eb008b;
-            margin-bottom: 2px;
-            width: 50%;
             margin: 0 auto 10px;
+            width: 50%;
             text-align: center;
             font-weight: 600;
         }
 
-        .chart-list.ab1 {
-            border-color: #003c6c;
-        }
-
         .chart-list h4 {
             color: #fff;
-            padding: 5px 10px 3px;
+            padding: 5px 10px;
             font-size: 24px;
-            border-top-left-radius: 7px;
             margin: 0;
-        }
-
-        .chart-list.ab1 h4 {
-            background-color: #024c88;
         }
 
         .chart-list a {
             display: block;
             font-size: 22px;
-            padding: 5px 7px 4px;
+            padding: 5px 7px;
             text-decoration: none;
-        }
-
-        .chart-list a:hover {
-            text-decoration: none;
-        }
-
-        .chart-list.ab1 a {
-            border-bottom: 2px solid #024c88;
-            color: #003c6c;
-        }
-
-        .chart-list a:hover {
-            background-color: #fff;
-            text-decoration: underline;
-        }
-
-        @media only screen and (max-width: 500px) {
-            .chart-list {
-                width: 95%;
-            }
         }
 
         footer {
@@ -194,298 +68,31 @@
             color: red;
             font-weight: bold;
             font-size: 25px;
-            text-decoration: none;
             border: 4px groove purple;
-            text-shadow: 1px 1px gold;
             margin: 3px;
         }
 
-        footer>div {
-            border-bottom: 2px solid #b2ddff;
-            padding: 10px 0;
-            margin-bottom: 10px;
-        }
-
-        footer>div a {
-            text-decoration: none;
-        }
-
-        footer>div a:hover {
-            text-decoration: none;
-        }
-
-        footer .ftr-icon {
-            text-decoration: none;
-            font-size: 35px;
-            text-transform: uppercase;
-            color: #007bff;
-        }
-
         footer p {
-            margin: 10px 0 10px;
+            margin: 10px 0;
             line-height: 35px;
-        }
-
-        footer p span {
-            color: rgb(51, 102, 255);
-        }
-
-        .panel.panel-info {
-            border: 1px solid #3f51b5;
-            width: 50%;
-            margin: 0 auto 0;
-        }
-
-        .panel-heading h1 {
-            margin: 0;
-            padding: 5px;
         }
 
         table {
             border-collapse: collapse;
+            width: 100%;
         }
 
         table,
         th,
         td {
             border: 1px solid #000;
-        }
-
-        thead {
-            background-color: #ffc107;
-            text-shadow: 1px 1px 2px #9a7400ab;
-        }
-
-        tbody td {
-            padding: 5px 0;
-            font-size: 24px;
-        }
-
-        .red,
-
-
-        @media only screen and (max-width: 770px) {
-            .panel.panel-info {
-                width: 70%;
-            }
-        }
-
-        @media only screen and (max-width: 500px) {
-            .panel.panel-info {
-                width: 100%;
-            }
-        }
-
-        .r {
-            color: red;
-        }
-
-        td {
-            font-weight: 700;
-        }
-
-        @media only screen and (max-width: 500px) {
-            .chart-list {
-                width: 95%;
-            }
-        }
-
-        .mp-btn {
-            position: fixed;
-            bottom: 9px;
-            left: 5px;
-            padding: 5px 8px;
-            font-size: 15px;
-            border: 1px solid #fff;
-            text-decoration: none;
-            background-color: #039;
-            color: #fff;
-        }
-
-        .para3 {
-            background: linear-gradient(187deg, #fc9 50%, #ffc387 50%);
-            border: 2px solid #ff0016;
-            border-top-style: solid;
-            border-right-style: solid;
-            border-bottom-style: solid;
-            border-left-style: solid;
-            border-style: outset;
-            margin-bottom: 3px;
-            line-height: 1.4;
-            font-size: 14px;
-            padding: 4px 10px;
-            color: #00094d;
-            text-shadow: 1px 1px 2px #fff;
-            box-shadow: 0 0 20px 0 rgb(0 0 0 / 40%);
-        }
-
-        .chart-h1 {
-            background: #ff00a2;
-            padding: 5px 10px;
-            text-shadow: 1px 1px 2px #000;
-            display: block;
-            color: #fff8f8;
-            margin-bottom: 3px;
-            letter-spacing: 1px;
-            font-weight: 700;
-            border: 2px solid #fff;
-            transform-style: preserve-3d;
-            transition: transform 150ms cubic-bezier(0, 0, 0.58, 1),
-                background 150ms cubic-bezier(0, 0, 0.58, 1);
-            font-size: 18px;
-            margin: 3px 0px;
-        }
-
-        h1 {
-            color: #fff8f8;
-        }
-
-        .logo img {
-            margin: 5px 0px;
-        }
-
-        h3 {
-            font-size: 16px;
-            color: #fff;
-            text-shadow: 0px 0px;
-            margin: 0px;
             padding: 5px;
-        }
-
-        .chart-result {
-            margin: 6px 2px;
-            line-height: 1.4;
-            font-size: 14px;
-            padding: 4px 10px;
-            color: #00094d;
-            text-shadow: 1px 1px 2px #fff;
-            box-shadow: 0 0 20px 0 rgb(0 0 0 / 40%);
-            border: 1px solid black;
-        }
-
-        .chart-result div {
-            font-size: 22px;
-            color: #00094d;
-            text-shadow: 1px 1px 2px #fff;
-        }
-
-        .chart-result span {
-            color: #880e4f;
-            text-shadow: 1px 1px 2px #ffe2c6;
-            font-size: 21px;
-        }
-
-        .chart-result a {
-            border: 1px solid #e6e6e6;
-            background: #522f92;
-            color: #fff;
-            padding: 5px 7px;
-            font-size: 12px;
-            margin: 2px 0 -1px;
-            display: inline-block;
-            transition: all 0.3s;
-            cursor: pointer;
-            text-shadow: none;
-            text-decoration: none;
-        }
-
-        @media screen and (max-width: 400px) {
-            .logo img {
-                height: 60px;
-                width: auto;
-                max-width: 100%;
-            }
-        }
-
-        @media screen and (max-width: 300px) {
-            .logo img {
-                height: 40px;
-                width: auto;
-                max-width: 100%;
-            }
-        }
-
-        .jdc-div {}
-
-        .jd-chat-h2 {
-            font-size: 18px;
-            color: #00094d;
-            text-shadow: 1px 1px 2px #fff;
-            margin: 5px;
-        }
-
-        .jd-chart-p {
-            font-size: 13px;
-            font-weight: 400;
-            margin: 0 0 10px;
-            padding: 0 10px 0;
-        }
-
-        .chart-faq {
-            font-size: 14px;
-            outline: 4px #fff;
-            outline-offset: -9px;
-            border: 2px solid #5c3411;
-            border-style: outset;
-            border-radius: 0;
-            padding: 0;
-            width: 50%;
-            margin: auto;
-        }
-
-        .faq-title {
-            background: #5c3411;
-            padding: 5px 10px;
-            text-shadow: 1px 1px 2px #000;
-            display: block;
-            color: #fff;
-            margin-bottom: 3px;
-            letter-spacing: 1px;
-            font-weight: 700;
-            margin: 0 0 5px;
-            font-size: 18px;
-        }
-
-        .faq-sub-title {
-            cursor: pointer;
-            color: #0013a5;
-            text-shadow: 1px 1px 2px #fff;
-            font-weight: 800;
-            font-size: 15px;
-        }
-
-        .faq-p {
-            color: #000;
-            font-weight: 700;
-            margin: 0;
-            font-size: 12px;
-            font-style: normal;
-            padding: 0 10px 5px;
-            opacity: 0.9;
-        }
-
-        @media only screen and (max-width: 770px) {
-            .chart-faq {
-                width: 70%;
-            }
-        }
-
-        @media only screen and (max-width: 500px) {
-            .chart-faq {
-                width: 100%;
-            }
-        }
-
-        @media only screen and (max-width: 500px) {
-            .chart-list {
-                width: 95%;
-            }
+            text-align: center;
         }
     </style>
 </head>
 
 <body>
-
     <div class="container-fluid">
         <div>
             <h1 class="chart-h1">{{ ucwords($categories->name) }}</h1>
@@ -566,14 +173,6 @@
         </div>
         <div class="clear">&nbsp;</div>
     </div>
-
-
-
-
-
-
-
-
 </body>
 
 </html>
